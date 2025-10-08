d727fc94e7fb3a98d1280ec82601a0809a99ab30
Zachary Scheer <zscheer00@gmail.com>
2025-10-08 11:26:52 -0700

    Adding single file for work practice, implemeting add method'

---
+++ main.py
@@ -0,0 +1,21 @@
+#python calc
+
+def add(a, b):
+    return int(a+b)
+
+
+
+operator = input("Enter an operator + - * /: ")
+
+num1 = float(input(("enter the 1st num: ")))
+num2 = float(input("enter the second number: "))
+
+
+
+if operator == "+":
+    print(add(num1,num2))
+
+
+
+
+
