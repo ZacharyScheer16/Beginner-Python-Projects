9dcf7f3056377a3815150483f09728b9c52660f4
Zachary Scheer <zscheer00@gmail.com>
2025-10-08 11:35:03 -0700

    multiplication and division, invalid error as well

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ def add(a, b):
 def remove(num1, num2):
     return int(num1-num2)
 
+def mult(n, b):
+    return int(n*b)
+
 
 
 operator = input("Enter an operator + - * /: ")
@@ -13,10 +16,21 @@ operator = input("Enter an operator + - * /: ")
 num1 = float(input(("enter the 1st num: ")))
 num2 = float(input("enter the second number: "))
 
+
+def divide(num1, num2):
+    return float(num1/num2)
+
+
 if operator == "+":
     print(add(num1,num2))
 elif operator == "-":
     print(remove(num1,num2))
+elif operator == "*":
+    print(mult(num1,num2))
+elif operator == "/":
+    print(divide(num1,num2))
+else:
+    print(f"{operator} is not valid")
 
 
 
