6b44f3064cbe174161ff9e48d5deb717e4a955fd
Zachary Scheer <zscheer00@gmail.com>
2025-10-08 11:30:20 -0700

    Subtraction method

--- main.py
+++ main.py
@@ -3,6 +3,9 @@
 def add(a, b):
     return int(a+b)
 
+def remove(num1, num2):
+    return int(num1-num2)
+
 
 
 operator = input("Enter an operator + - * /: ")
@@ -10,10 +13,10 @@ operator = input("Enter an operator + - * /: ")
 num1 = float(input(("enter the 1st num: ")))
 num2 = float(input("enter the second number: "))
 
-
-
 if operator == "+":
     print(add(num1,num2))
+elif operator == "-":
+    print(remove(num1,num2))
 
 
 
