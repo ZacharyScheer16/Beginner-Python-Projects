efd7eea8b9e44297e08dcbf5547d4abd38ec67ec
Zachary Scheer <zscheer00@gmail.com>
2025-11-16 08:52:35 -0700

    Firs time using Numpy

---
+++ BroCodeNumpy/Practice1.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+my_list = [1,2,3,4]
+
+my_listNormal = my_list*2
+my_ListNumpy = np.array(my_list)
+my_ListNumpyMultiplied = my_ListNumpy *2
+
+print(my_list)
+print(my_ListNumpy)
+print(my_ListNumpyMultiplied)
