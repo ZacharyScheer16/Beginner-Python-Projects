023f6de95c208d0e09fdaaf23f8ce9e56885a942
Zachary Scheer <zscheer00@gmail.com>
2025-12-28 16:34:43 -0700

    commit

---
+++ 30DayCodingChallenge/Day17ExceptionHandling.py
@@ -0,0 +1,32 @@
+#Try exception handling
+'''
+try:
+    print(10 + ('5'))
+except:
+    print('Something went wrong')
+
+try:
+    name = input('Enter your name:')
+    year_born = input('Year you born:')
+    age = 2019 - int(year_born)
+    print(f'You are {name}. And your age is {age}.')
+except TypeError:
+    print('Type error occur')
+except ValueError:
+    print('Value error occur')
+except ZeroDivisionError:
+    print('zero division error occur')
+else:
+    print('I usually run with the try block')
+finally:
+    print('I alway run.')
+
+try:
+    name = input('Enter your name:')
+    year_born = input('Year you born:')
+    age = 2019 - int(year_born)
+    print(f'You are {name}. And your age is {age}.')
+except Exception as e:
+    print(e)
+'''
+
